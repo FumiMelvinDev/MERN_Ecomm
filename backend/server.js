077ba4052b5461a11cf3e069051b6445f5c48dc5
FumiMelvinDev <fumimelvindev@gmail.com>
2023-11-08 16:15:28 +0200

    connect mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errMiddleware')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const port = process.env.PORT || 5000
 
+connectDB()
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-errorHandler()
+app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
